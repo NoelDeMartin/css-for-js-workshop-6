ff8fb785d2ed40c68b21dcb2c0f8f8e8987c7a36
Noel De Martin <noel@noeldemartin.com>
2023-10-11 17:34:32 +0200

    Define alt attributes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,9 @@ const Hero = () => {
             /images/hero-img@3x.jpg 3x
           "
         />
-        <HeroImage src="/images/hero-img.jpg" />
+        <HeroImage src="/images/hero-img.jpg" alt="A welcoming cat watching over your collection of images" />
       </picture>
-      <Swoop src="/swoop.svg" />
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
